f5da8deb3e9e849ced6d4d99a34a539c30631322
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Create clients model and migration

---
+++ database/migrations/2021_12_10_175643_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('address')->comment('get address from google map api');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
